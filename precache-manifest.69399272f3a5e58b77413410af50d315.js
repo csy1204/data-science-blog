6cbe7446815a4fc927889c68c012e2295cd9d861
sy <josang1204@gmail.com>
2020-04-01 14:35:01 +0900

    Updates

---
+++ precache-manifest.69399272f3a5e58b77413410af50d315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2c76b5f8ffd0ad43ad9fd3fd0ba884",
+    "url": "/data-science-blog/index.html"
+  },
+  {
+    "revision": "ad300ed2458c5a054240",
+    "url": "/data-science-blog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8909955ac269fc352c1e",
+    "url": "/data-science-blog/static/js/2.675fa13d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/data-science-blog/static/js/2.675fa13d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad300ed2458c5a054240",
+    "url": "/data-science-blog/static/js/main.d00bcfd3.chunk.js"
+  },
+  {
+    "revision": "bef115c1cc4b6a237dfc",
+    "url": "/data-science-blog/static/js/runtime-main.8d50559b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/data-science-blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
