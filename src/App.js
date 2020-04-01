3ca0b2581beeac29a9bb98eb83a413bba69340b3
sy <josang1204@gmail.com>
2020-04-01 15:06:20 +0900

    edit main page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+         github action 테스트 중
+	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
